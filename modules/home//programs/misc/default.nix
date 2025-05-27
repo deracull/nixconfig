b9bad72edf36f24179122251bfcb047582e3219d
deracul <identificationsucks@gmail.com>
2025-05-27 20:59:47 +1000

    removed useless apps

--- modules/home//programs/misc/default.nix
+++ modules/home//programs/misc/default.nix
@@ -4,13 +4,11 @@
     homeDirectory = "/home/${user.username}";
     packages = with unstable; [
       vlc
-      gimp3
       wayvnc
       helvum
       parsec-bin
       easyeffects
       pavucontrol
-      davinci-resolve
       signal-desktop-bin
       ungoogled-chromium
     ];
